62f9a27f9b2d5054f20ec2ba49794aa6d7dd8c06
Jaikrat Singh <tektutorialfeedback@gmail.com>
2021-07-26 22:35:35 +0530

    Initial commit

---
+++ src/main/java/com/spring/solace/consumer/SpringSolaceConsumerApplication.java
@@ -0,0 +1,28 @@
+package com.spring.solace.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.activation.MimeType;
+
+@SpringBootApplication
+@RestController
+public class SpringSolaceConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSolaceConsumerApplication.class, args);
+    }
+
+    @PostMapping("/rest/tutorials")
+    public String sayHello()  {
+        return "Hello From Spring and Kubernetes example";
+    }
+
+    @PostMapping(value="/T/rest/pubsub", consumes = MediaType.TEXT_PLAIN_VALUE)
+    public void postHello(@RequestBody String str)  {
+        System.out.println("Request:- " + str);
+        //return "Hello From Spring and Kubernetes example";
+    }
+}
---
+++ src/test/java/com/spring/solace/consumer/SpringSolaceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.solace.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSolaceConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
